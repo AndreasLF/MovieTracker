613b8f2e7c61a9a5ec2639aee69e38437cb1c31f
AndreasLF <andreas@lyhnefiehn.dk>
2018-03-28 16:12:44 +0200

    CHANGE: Created register user page and PHP-script
    POC:
    Created testuser

---
+++ register.php
@@ -0,0 +1,50 @@
+    <?php
+        
+    //Starts the session
+    session_start(); 
+
+    //Connects to the database by including the code from connection.php
+    require 'connection.php'; 
+
+
+    /*if 'username' and 'password' received from the HTML form is set, the following code will run*/
+    if(isset($_POST['username']) and isset($_POST['password'])){
+        
+        /*This trims the username and password. Trim removes any spaces from the end or the beginning of a string*/
+        $username = trim($_POST['username']); 
+        $password = trim($_POST['password']);
+        
+        
+        //Creates a prepared statement for the database
+        $stmt = mysqli_prepare($connection,"INSERT INTO login(Username,Password) VALUES (?,?)");
+            
+        //Binds parameters to the prepared statement. Every parameter is of type String
+        $stmt->bind_param("ss",$username,$password); 
+
+        //Executes the prepared statement. Returns a boolean - true on succes and false on failure.
+        $result = $stmt->execute(); 
+        
+        //If $result is true (mysqli_query was unsuccesful)
+        if ($result){ 
+            //This redirects the browser to login.html
+            header('Location: login.html'); 
+        }
+        else{ 
+            //An error message is created and stored in the session variable
+            exit("ERROR executing: $query"."<br>". mysqli_error($result)); 
+        }
+        
+    }
+    else{
+        exit("ERROR");
+    }
+    ?>
+
+
+
+<html>
+    <body>
+    
+    
+    </body>
+</html>
