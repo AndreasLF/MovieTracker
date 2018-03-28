33fb2b19238eca5f07dd745381d1810502852fd7
AndreasLF <andreas@lyhnefiehn.dk>
2018-03-28 16:47:12 +0200

    CHANGE: Created login page and PHP-script
    POC: Tested login

---
+++ login.php
@@ -0,0 +1,51 @@
+    <?php
+        
+    //Starts the session
+    session_start(); 
+
+    //Connects to the database by including the code from connection.php
+    require 'connection.php'; 
+
+    /*If the session variable 'username' is set, the username and password will be checked against the database*/
+    if(isset($_POST['username']) and isset($_POST['password'])){
+        
+        /*This trims the username and password. Trim removes any spaces from the end or the beginning of a string*/
+        $username = trim($_POST['username']); 
+        $password = trim($_POST['password']);
+        
+        //Creates a prepared statement for the database
+        $stmt = mysqli_prepare($connection,"SELECT Password FROM `login` WHERE Username=?");
+            
+        //Binds parameters to the prepared statement. Every parameter is of type String
+        $stmt->bind_param("s",$username); 
+
+        //Executes the prepared statement. Returns a boolean - true on succes and false on failure.
+        $result = $stmt->execute();
+        
+        $stmt->bind_result($pass);
+       
+        $stmt->fetch();      
+    
+                    
+        if($pass == $password){  
+            echo "valid login";
+        }
+        else{
+            
+            echo "invalid login";
+        }
+        
+        
+        
+        
+    }
+    ?>
+
+
+
+<html>
+    <body>
+    
+    
+    </body>
+</html>
