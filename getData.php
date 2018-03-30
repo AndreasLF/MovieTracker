cb0c27b356843dafe9d318c311c8003083c3d2ad
AndreasLF <andreas@lyhnefiehn.dk>
2018-03-30 09:46:31 +0200

    CHANGE: Modified results from omdb to contain ...
    Modified results from omdb to contain a boolean. The boolean is supposed
    to tell if the movie is in the users list.
    
    POC:
    Looked at the returned json file to see if the boolean had been added.
    Forced a search which didn't have any response to test if this still
    works. Succes

--- getData.php
+++ getData.php
@@ -5,8 +5,8 @@ $apikey = "b044049a";
 $type = "movie";
 
 //replaces spaces with +
-$title = preg_replace("/\s/","+",trim($_GET['title']));
-
+//$title = preg_replace("/\s/","+",trim($_GET['title']));
+$title = "Tomb+Raider";
 
 $movieUrl = "https://www.omdbapi.com/?apikey=".$apikey."&s=".$title."&type=".$type;
 
@@ -14,6 +14,33 @@ $movieUrl = "https://www.omdbapi.com/?apikey=".$apikey."&s=".$title."&type=".$ty
 $json = file_get_contents($movieUrl);
 $movieObj = json_decode($json);
 
+
+//If the response from OMDB contains movies it will be modified to contain a boolean which tells whether or not each movie is in the database 
+if($movieObj->Response == "True"){
+    
+    $newMovieObj = array(
+        "Response" => $movieObj->Response,
+        "totalResults" => $movieObj->totalResults
+    );
+        
+    $searchResults;
+    
+    foreach($movieObj->Search as $key=>$movie){
+
+        
+        //Converts the object to an array, adds a parameter and converts it back to an object
+        $movie = (object) array_merge((array)$movie,array('isInList'=>true));
+        //var_dump($movieElement);  
+        $searchResults[$key] = $movie;
+    }
+    
+    $newMovieObj['Search'] = $searchResults;
+    $json = json_encode($newMovieObj);
+}
+
+
 echo $json;
+
+
 
 ?>
