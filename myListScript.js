e6236aacd2db672522024c01691f08a901dd9336
AndreasLF <andreas@lyhnefiehn.dk>
2018-03-30 17:50:46 +0200

    CHANGE: Get statistics from server AJAX
    POC: Printed to console

--- myListScript.js
+++ myListScript.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
     
     
-/*
-    $.getJSON("checkLogin.php", function (data) {
-            if(data.loggedin){
-                Materialize.toast("Welcome "+data.username+"! This is your list of movies",2000);
-            }
-            else{
-                window.location.replace("index.html");
-            }
+
+    $('#statisticsButton').click(function(){
+     
+       $.getJSON("getStatistics.php", function (data) {
+            console.log(data);
+    }); 
+        
     });
     
-    */
+    
+    
     //Initiates modals
     $('.modal').modal();
 });
