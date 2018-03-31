7778924ac837feee95e0aa5f68ff7f35bf7e92a3
AndreasLF <andreas@lyhnefiehn.dk>
2018-03-31 12:03:29 +0200

    REFAC: Added error msg and changed to OOP-style

--- register.php
+++ register.php
@@ -11,54 +11,64 @@
 
     /*if 'username' and 'password' received from the HTML form is set, the following code will run*/
     if(isset($_POST['username']) and isset($_POST['password'])){
-        
+
         /*This trims the username and password. Trim removes any spaces from the end or the beginning of a string*/
         $username = trim($_POST['username']); 
         $password = trim($_POST['password']);        
-        
+
         //Creates password hash
         $passwordHash = password_hash($password, PASSWORD_BCRYPT);
 
-        
-        
+
+
         //Creates a prepared statement for the database
-        $stmt = mysqli_prepare($connection,"INSERT INTO login(Username,Password) VALUES (?,?)");
-            
+        $stmt = $mysqli->prepare("INSERT INTO login(Username,Password) VALUES (?,?)");
+
+        //If the prepared statement fails to be defined the script will exit with an error message
+        if(!($stmt)){
+            //Sets the error message
+            //htmlspecialchars converts the characters into html enitities
+            exit("mysqli_prepare failed: " . htmlspecialchars($mysqli->error));
+        }
+
+
         //Binds parameters to the prepared statement. Every parameter is of type String
-        $stmt->bind_param("ss",$username,$passwordHash); 
-        
+        $result = $stmt->bind_param("ss",$username,$passwordHash);
+
+        if(!($result)){
+            //Exits with an error message
+            exit("mysqli bind_param failed: " . htmlspecialchars($stmt->error));
+        }
+
 
         //Executes the prepared statement. Returns a boolean - true on succes and false on failure.
         $result = $stmt->execute(); 
-        
-        
+
+         if(!($result)){
+            //Exits with an error message
+            exit("mysqli exceute failed: " . htmlspecialchars($stmt->error));
+        }
+
+
          //A query for a new database table is created
         $query = "CREATE TABLE ".$username." (imdbId VARCHAR(10) PRIMARY KEY, json JSON NOT NULL)"; 
 
-        
-        //If $result is true (mysqli_query was unsuccesful)
-        if ($result){ 
-            
-           
-            //The query is performed
-            $result = mysqli_query($connection, $query); 
 
-            if($result){
-               //This redirects the browser to login.html
-                header('Location: index.html');  
-            }
-            else{
-                //An error message is created and stored in the session variable
-                $errorMsg = mysqli_error($connection);
-            }
-            
-            
+
+        //The query is performed
+        $result = $mysqli->query($query); 
+
+
+        if($result){
+           //This redirects the browser to login.html
+            header('Location: index.html');  
         }
-        else{ 
+        else{
             //An error message is created and stored in the session variable
-            $errorMsg = mysqli_error($connection);
+            exit("mysqli query failed: " . htmlspecialchars($stmt->error));
         }
-        
+
+
     }
     else{
         $errorMsg = "You cannot register a user without providing a username and password";
